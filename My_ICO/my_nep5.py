ca1e54a0ea21360f6ba0b9fa554c7c2c16e3e4ae
saeed-automata <37187953+saeed-automata@users.noreply.github.com>
2018-06-10 06:22:57 +0500

    Add files via upload

--- My_ICO/my_nep5.py
+++ My_ICO/my_nep5.py
@@ -3,6 +3,7 @@ from boa.interop.Neo.Action import RegisterAction
 # from boa.interop.Neo.Storage import *
 # from boa.builtins import concat
 from My_ICO.my_token import *
+from boa.interop.Neo.Runtime import CheckWitness
 
 OnTransfer = RegisterAction('transfer', 'addr_from', 'addr_to', 'amount')
 OnApprove = RegisterAction('approve', 'addr_from', 'addr_to', 'amount')
